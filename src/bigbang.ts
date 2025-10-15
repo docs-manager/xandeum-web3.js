646d17552902efdf154e1b490c25a830d9f98583
Isha Mandaviya <mandviyaisha@gmail.com>
2025-10-15 11:18:27 +0530

    add replica_count to the bigbang

--- src/bigbang.ts
+++ src/bigbang.ts
@@ -1,4 +1,5 @@
 import { Transaction, TransactionInstruction, PublicKey } from '@solana/web3.js'
+import BN from 'bn.js'
 import { programId } from './const'
 
 /**
@@ -7,9 +8,10 @@ import { programId } from './const'
  * @param wallet - The public key of the wallet that will sign and authorize the transaction.
  * @returns A Promise that resolves to a Solana `Transaction` object containing the bigbang instruction.
  */
-export async function bigbang (wallet: PublicKey): Promise<Transaction> {
+export async function bigbang (replica_count:string,wallet: PublicKey): Promise<Transaction> {
   const instructionData = Buffer.concat([
-    Buffer.from(Int8Array.from([0]).buffer)
+    Buffer.from(Int8Array.from([0]).buffer),
+    Buffer.from(Uint8Array.of(...new BN(replica_count).toArray('le', 8)))
   ])
 
   const instruction = new TransactionInstruction({
